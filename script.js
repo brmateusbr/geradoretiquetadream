73061ea10831d7dfd09390a69051b2b9cf75d24c
brmateusbr <matheusborges@Lojasdream.com>
2024-09-20 13:27:16 -0300

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ function adicionarProduto() {
 
     celulaEAN.innerHTML = '<input type="text" class="ean-input" placeholder="EAN">';
     celulaNome.innerHTML = '<input type="text" class="nome-input" placeholder="Nome" readonly>';
-    celulaPreco.innerHTML = '<input type="text" class="preco-input" value="0.00" readonly>';
+    celulaPreco.innerHTML = '<input type="text" class="preco-input" placeholder="preco" readonly>';
     celulaQuantidade.innerHTML = '<input type="number" class="quantidade-input" min="1" value="1">';
     celulaAcoes.innerHTML = '<button class="removerProduto">Remover</button>';
 
